03c501fa92b5c7d6f2db90e4ba911c4e603877ba
ibqrz <isabelqa77@gmail.com>
2025-05-12 17:55:30 -0300

    k

--- integration-test/cypress/e2e/voltar.cy.js
+++ integration-test/cypress/e2e/voltar.cy.js
@@ -4,7 +4,7 @@ describe('Entrar em Login de voltar a Página Inicial', () => {
     });
 
     it('Deve voltar a página inicial', () => {
-        cy.get('div a').contains('Cosmovisão').click();
+        cy.get('div a').contains('voltar').click();
         cy.url().should('include', 'index.html');
     });
 });
